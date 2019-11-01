d8babb834a95a3d3f36ab5a4f5ffa9f33c44945b
Chris Sng <chrissng@users.noreply.github.com>
2019-11-01 09:39:39 +0800

    Consul exporter for Prometheus (#21)
    * Consul exporter for Prometheus
    
    * one replica, prom scrape annotation
    
    * depends on
    
    * expose service annotations

---
+++ modules/consul/exporter.tf
@@ -0,0 +1,34 @@
+# Deploy Consul Exporter for Prometheus
+resource "helm_release" "consul_exporter" {
+  count      = var.enable_exporter ? 1 : 0
+  depends_on = [helm_release.consul]
+
+  name       = var.exporter_release_name
+  chart      = var.exporter_chart_name
+  repository = var.exporter_chart_repository
+  version    = var.exporter_chart_version
+  namespace  = var.chart_namespace
+
+  values = [
+    data.template_file.exporter_values[0].rendered,
+  ]
+}
+
+data "template_file" "exporter_values" {
+  count = var.enable_exporter ? 1 : 0
+
+  template = file("${path.module}/templates/exporter-values.yaml")
+
+  vars = {
+    replica = var.exporter_replica
+    image   = var.exporter_image
+    tag     = var.exporter_tag
+
+    resources = jsonencode(var.exporter_resources)
+    affinity  = jsonencode(var.exporter_affinity)
+
+    consul_server_and_port = "consul-server.${var.chart_namespace}.svc.cluster.local:8500"
+
+    service_annotations = indent(4, var.exporter_service_annotations)
+  }
+}
