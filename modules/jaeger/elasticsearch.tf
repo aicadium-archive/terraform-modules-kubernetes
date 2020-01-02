549c8c261408744f6886aae2c610c0338e621bc8
Chris Sng <chrissng@users.noreply.github.com>
2020-01-02 18:56:50 +0800

    prepare master deployment

--- modules/jaeger/elasticsearch.tf
+++ modules/jaeger/elasticsearch.tf
@@ -1,3 +1,17 @@
+resource "helm_release" "elasticsearch_data" {
+  count = local.elasticsearch_enabled
+
+  name       = "${var.elasticsearch_release_name}-data"
+  chart      = var.elasticsearch_chart_name
+  repository = var.elasticsearch_chart_repository
+  version    = var.elasticsearch_chart_version
+  namespace  = local.elasticsearch_namespace
+
+  values = [
+    data.template_file.elasticsearch_data[0].rendered,
+  ]
+}
+
 resource "helm_release" "elasticsearch" {
   count = local.elasticsearch_enabled
 
@@ -16,6 +30,21 @@ data "template_file" "elasticsearch" {
   count = local.elasticsearch_enabled
 
   template = file("${path.module}/templates/elasticsearch.yaml")
+
+  vars = {
+    es_client_resources           = jsonencode(var.jaeger_es_client_resources)
+    es_master_resources           = jsonencode(var.jaeger_es_master_resources)
+    es_data_replicas              = var.jaeger_es_data_replicas
+    es_data_resources             = jsonencode(var.jaeger_es_data_resources)
+    es_data_persistence_disk_size = var.jaeger_es_data_persistence_disk_size
+  }
+}
+
+
+data "template_file" "elasticsearch_data" {
+  count = local.elasticsearch_enabled
+
+  template = file("${path.module}/templates/elasticsearch_data.yaml")
 
   vars = {
     es_client_resources           = jsonencode(var.jaeger_es_client_resources)
