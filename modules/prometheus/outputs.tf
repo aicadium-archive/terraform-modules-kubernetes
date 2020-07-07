09f161c305df300236a950690b04ceaa8f64ad18
Chris Sng <chrissng@users.noreply.github.com>
2020-07-07 16:28:18 +0800

    output alerts api url

--- modules/prometheus/outputs.tf
+++ modules/prometheus/outputs.tf
@@ -12,3 +12,8 @@ output "prometheus_query_api_url" {
   description = "Prometheus query API URL: https://prometheus.io/docs/prometheus/latest/querying/api/#expression-queries"
   value       = local.prometheus_query_api_url
 }
+
+output "prometheus_alerts_api_url" {
+  description = "Prometheus query API URL: https://prometheus.io/docs/prometheus/latest/querying/api/#expression-queries"
+  value       = local.prometheus_alerts_api_url
+}
